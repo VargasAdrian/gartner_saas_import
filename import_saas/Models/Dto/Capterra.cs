0f231a1698aab76a35f4294f256da8acddf0cae6
avargas <avargas@vocorp.com>
2022-10-08 20:02:49 +0000

    Added file models (Capterra and Software Advice)

---
+++ import_saas/Models/Dto/Capterra.cs
@@ -0,0 +1,8 @@
+namespace import_saas.Models.Dto;
+
+public class Capterra
+{
+    public string name { get; set; }
+    public string? twitter { get; set; }
+    public string tags { get; set; }
+}
---
+++ import_saas/Models/Dto/SoftwareAdvice.cs
@@ -0,0 +1,8 @@
+namespace import_saas.Models.Dto;
+
+public class SoftwareAdvice
+{
+    public string title { get; set; }
+    public string? twitter { get; set; }
+    public string categories { get; set; }
+}
