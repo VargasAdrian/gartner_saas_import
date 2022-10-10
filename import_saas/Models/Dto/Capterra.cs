2b2a818e65c3c38d783eaa01b1a06168cee56a3f
avargas <avargas@vocorp.com>
2022-10-10 13:19:35 +0000

    Added required attribute to dto models

--- import_saas/Models/Dto/Capterra.cs
+++ import_saas/Models/Dto/Capterra.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace import_saas.Models.Dto;
 
 public class Capterra
 {
+    [Required]
     public string name { get; set; }
     public string? twitter { get; set; }
+    [Required]
     public string tags { get; set; }
 }
--- import_saas/Models/Dto/SoftwareAdvice.cs
+++ import_saas/Models/Dto/SoftwareAdvice.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace import_saas.Models.Dto;
 
 public class SoftwareAdvice
 {
+    [Required]
     public string title { get; set; }
     public string? twitter { get; set; }
+    [Required]
     public IEnumerable<string> categories { get; set; }
 }
