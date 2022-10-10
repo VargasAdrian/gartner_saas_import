f5bc123ddefbc2d7b512193edcb129c211bd9dce
avargas <avargas@vocorp.com>
2022-10-10 15:42:13 +0000

    Removed required attribute from Capterra model

--- import_saas/Models/Dto/Capterra.cs
+++ import_saas/Models/Dto/Capterra.cs
@@ -4,9 +4,7 @@ namespace import_saas.Models.Dto;
 
 public class Capterra
 {
-    [Required]
     public string name { get; set; }
     public string? twitter { get; set; }
-    [Required]
     public string tags { get; set; }
 }
