7658a8d8fcc17763fea08f11fd9f2a372b94bf23
avargas <avargas@vocorp.com>
2022-10-10 15:42:25 +0000

    Added mapping test capterra

--- import_test/MapperTest.cs
+++ import_test/MapperTest.cs
@@ -1,12 +1,47 @@
+using AutoMapper;
+using import_saas.Mapper;
+using import_saas.Models.Db.MySql;
+using import_saas.Models.Dto;
+
 namespace import_test;
 
 public class MapperTest
 {
-    // [Fact]
-    // public void CapterraModel()
-    // {
+    private IMapper InitMapper()
+    {
+        return new MapperConfiguration(opt => 
+        {
+            opt.AddProfile(new ProductProfile());
+        }).CreateMapper();
+    }
 
-    // }
+    [Fact]
+    public void CapterraModel()
+    {
+        var mapper = InitMapper();
+
+        var capterraData = new List<Capterra>()
+        {
+            new Capterra
+            {
+                name = "a_name1",
+                twitter = "a_twitter1",
+                tags = "a_tag1,a_tag2,a_tag3"
+            },
+            new Capterra
+            {
+                name = "b_name2",
+                twitter = "b_twitter2",
+                tags = "b_tag1,b_tag2"
+            },
+        };
+
+        var products = mapper.Map<List<Capterra>, List<Product>>(capterraData);
+
+        Assert.Equal<int>(3, products.First().categories.Count());
+        Assert.True("a_name1".Equals(products.First().name));
+        Assert.True("b_twitter2".Equals(products[1].twitter));
+    }
 
     // [Fact]
     // public void SoftwareAdviceModel()
